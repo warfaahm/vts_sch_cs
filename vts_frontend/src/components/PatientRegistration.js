9821dd7d5cec32347be0c7bfc73460a3c21d0b23
Warfa Ahmed <warfaahmed25@gmail.com>
2023-03-29 23:50:35 +0300

    first commit

--- vts_frontend/src/components/PatientRegistration.js
+++ vts_frontend/src/components/PatientRegistration.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from "../assets/img/logo.png";
 
 function PatientRegistration() {
+    document.title = "Patient Registration | VTS";
     return (
         <div className="bg-white h-screen">
             <div className=" flex justify-center items-center">
@@ -20,35 +21,35 @@ function PatientRegistration() {
                             <div class="grid gap-4 mb-2 md:grid-cols-2">
                                 <div className="mb-2 w-full">
                                     <label htmlFor="first_name" className="label_1">First name</label>
-                                    <input type="text" id="first_name" name="first_name" className="input_1"/>
+                                    <input type="text" id="first_name" name="first_name" className="input_1" required/>
                                 </div>
                                 <div className="mb-2 w-full">
                                     <label  htmlFor="last_name" className="label_1">Last name</label>
-                                    <input type="text" id="last_name" name="last_name" className="input_1"/>
+                                    <input type="text" id="last_name" name="last_name" className="input_1" required/>
                                 </div>
                                 <div className="mb-2 w-full">
                                     <label htmlFor="id_no" className="label_1">ID Number</label>
-                                    <input type="text" id="id_no" name="id_no" className="input_1"/>
+                                    <input type="text" id="id_no" name="id_no" className="input_1" required/>
                                 </div>
                                 <div className="mb-2 w-full">
                                     <label  htmlFor="phone_no" className="label_1">Phone No</label>
-                                    <input type="text" id="phone_no" name="phone_no" className="input_1"/>
+                                    <input type="text" id="phone_no" name="phone_no" className="input_1" required/>
                                 </div>
                             </div>
                             <div>
                                 <h3 className="label_1">Gender</h3>
                                 <div className="inline">
-                                    <input type="radio" name="gender" id="gender" value="M" className="radio-form"/>
+                                    <input type="radio" name="gender" id="gender" value="M" className="radio-form" required/>
                                     <label className="mx-2">Male</label>
                                 </div>
                                 <div className="inline m-4">
-                                    <input type="radio" name="gender" id="gender" value="F" className="radio-form"/>
+                                    <input type="radio" name="gender" id="gender" value="F" className="radio-form" required/>
                                     <label className="mx-2">Female</label>
                                 </div>
                             </div>
                             <div className="my-4 w-full">
                                 <label htmlFor="dob" className="label_1">Date of birth</label>
-                                <input type="date" id="dob" name="dob" className="input_1"/>
+                                <input type="date" id="dob" name="dob" className="input_1" required/>
                             </div>
                             <div>
                                 <label  htmlFor="allergy" className="label_1">Allergy</label>
@@ -56,15 +57,15 @@ function PatientRegistration() {
                             </div>
                             <div className="my-4 w-full">
                                 <label htmlFor="email" className="label_1">Email</label>
-                                <input type="email" id="email" name="email" className="input_1"/>
+                                <input type="email" id="email" name="email" className="input_1" required/>
                             </div>
                             <div>
                                 <label  htmlFor="password" className="label_1">Password</label>
-                                <input type="password" id="password" name="password" className="input_1"/>
+                                <input type="password" id="password" name="password" className="input_1" required/>
                             </div>
                             <div>
                                 <label  htmlFor="password_confirmation" className="label_1">Confirm Password</label>
-                                <input type="password" id="password_confirmation" name="password_confirmation" className="input_1"/>
+                                <input type="password" id="password_confirmation" name="password_confirmation" className="input_1" required/>
                             </div>
                             <div className="my-2">
                                 <div className="inline">
